474dfc0670bb18d8cc67a8943b324ed011bcfc61
Talita Oliveira dos Santos <talitaosantos@yahoo.com.br>
2024-05-07 18:36:52 -0300

    feat - teste pi

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php 
-echo "<center><h1>Estou vivo!!</h1></center>"
-?>
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Olá mundo!</h1>
+</body>
+</html>
